5f7e8dab845385691157a262373f286e5591f385
exist <existqq@gmail.com>
2024-03-06 22:18:11 +0300

    Added a simple logger on start/disable

--- src/main/java/discordconnect/discordconnect/DiscordConnect.java
+++ src/main/java/discordconnect/discordconnect/DiscordConnect.java
@@ -1,5 +1,6 @@
 package discordconnect.discordconnect;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DiscordConnect extends JavaPlugin {
@@ -7,10 +8,12 @@ public final class DiscordConnect extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getLogger().info("DiscordConnector is working!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("DiscordConnector is turning off!");
     }
 }
