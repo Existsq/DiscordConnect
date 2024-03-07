0ba55c5a1aebeac2ee8b8e511bd023ec4c6916a5
exist <existqq@gmail.com>
2024-03-07 16:33:22 +0300

    created onMessageSent func

--- src/main/java/Handlers/ChatHandler.java
+++ src/main/java/Handlers/ChatHandler.java
@@ -1,4 +1,44 @@
 package Handlers;
 
-public class ChatHandler {
+import discordconnect.discordconnect.DiscordConnect;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatHandler implements Listener {
+
+    public ChatHandler(DiscordConnect plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+
+
+    @EventHandler
+    public void onMessageSent(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        
+
+    }
+
+
+    @EventHandler
+    public void onTorchPlace(BlockPlaceEvent event) {
+        Block block = event.getBlock();
+
+        if (block.getType() != Material.TORCH) {
+            return;
+        }
+
+        Bukkit.getLogger().info("A torch was placed!");
+        Player player = event.getPlayer();
+        player.setAllowFlight(true);
+    }
+
 }
