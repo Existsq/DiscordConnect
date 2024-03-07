5e9032a11b10fd19dff0fff15dd727f375997b3b
exist <existqq@gmail.com>
2024-03-07 11:03:40 +0300

    Updated JDA instance

--- src/main/java/DiscordBot/JDAInstance.java
+++ src/main/java/DiscordBot/JDAInstance.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class JDAInstance {
     public static void main(String[] args) {
-        
         try {
             JDA jda = JDABuilder.createDefault(Constants.TOKEN).build();
         } catch (Exception ignored) {
