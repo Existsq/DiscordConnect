49cb75f17d2a8370c64762f11ae99d1ecce6102d
exist <existqq@gmail.com>
2024-03-07 10:59:00 +0300

    Added simple Jda instance of bot

--- src/main/java/DiscordBot/JDAInstance.java
+++ src/main/java/DiscordBot/JDAInstance.java
@@ -1,4 +1,16 @@
 package DiscordBot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class JDAInstance {
+    public static void main(String[] args) {
+        
+        try {
+            JDA jda = JDABuilder.createDefault(Constants.TOKEN).build();
+        } catch (Exception ignored) {
+        }
+
+    }
+
 }
